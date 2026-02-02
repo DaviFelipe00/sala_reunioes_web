a868ed358140b67accdefbb07c1fa7fc5ce9441a
Davi Barbosa <davi.barbosa@rioave.com.br>
2026-02-02 16:21:19 -0300

    Evoluindo o código

--- Domain/Entities/CustomCalendarItem.cs
+++ Domain/Entities/CustomCalendarItem.cs
@@ -5,7 +5,6 @@ namespace SalaReunioes.Web.Domain.Entities;
 
 public class CustomCalendarItem : CalendarItem
 {
-    // Adicionamos a cor da sala e o nome para facilitar o acesso no template
     public Color SalaColor { get; set; } = Color.Default;
     public string NomeSala { get; set; } = string.Empty;
 }
