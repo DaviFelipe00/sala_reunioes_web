9ca7db4414696f0ba4db864fb18f5d5d769e57dd
davifelipe00 <davifelipeempregos@gmail.com>
2026-02-03 09:20:13 -0300

    visual Rio Ave

--- obj/Debug/net10.0/SalaReunioes.Web.AssemblyInfo.cs
+++ obj/Debug/net10.0/SalaReunioes.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalaReunioes.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b323d8edeea67df6c36336f8f7037ff9b6b29a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c4cbe13e5bcbd364ca8832c228e2da072293d94")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalaReunioes.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalaReunioes.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
